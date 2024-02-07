05c9ef4d9a72a140866ee2bbfd8a3d873d4eb2fa
yuki-koma3 <y_komatsu@3sunny.net>
2024-02-07 11:11:00 +0900

    feat:エラー出てたのでアクティブかどうかで切り分け

--- freee-extend/content.js
+++ freee-extend/content.js
@@ -1,30 +1,52 @@
 setTimeout(() => {
+    if (!isActivatePage()) {
+        console.log("freee-extend: not activated");
+        return;
+    }
     console.log("freee-extend: activated");
+    addAverageWorkHoursElement();
+    setEventOnChangeMonth();
+    // NOTE: mount 時に実行したいが、onloadではDOM がまだ生成されていないため、setTimeout で遅延させている
+}, 2000);
+
+function setEventOnChangeMonth() {
+    window.document.querySelectorAll(".month-link").forEach((element) => {
+            element.addEventListener("click", () => {
+                setTimeout(() => {
+                    addAverageWorkHoursElement();
+                }, 2000);
+            });
+        }
+    )
+}
+
+function isActivatePage() {
+    return window.document.querySelector('[aria-current="page"]').innerText === "勤怠編集";
+}
+
+
+function getWorkDays() {
     const workDaysElement = window.document.querySelector(".item .label");
     const workDaysText = workDaysElement.nextElementSibling.textContent;
-    const workDays = parseInt(workDaysText, 10); // 「日」を除去して数値に変換
+    return parseInt(workDaysText, 10); // 「日」を除去して数値に変換
+}
 
-// 総勤務時間を取得
-    const totalWorkHoursElement =
-        window.document.querySelectorAll(".items .label")[1].nextElementSibling;
-    const totalWorkHoursText =
-        totalWorkHoursElement.querySelector(".hour-min__value").textContent;
-    const totalWorkHours = parseInt(totalWorkHoursText, 10); // 「時間」を除去して数値に変換
+function getTotalWorkHours() {
+    const totalWorkHoursElement = window.document.querySelectorAll(".items .label")[1].nextElementSibling;
+    const totalWorkHoursText = totalWorkHoursElement.querySelector(".hour-min__value").textContent;
+    return parseInt(totalWorkHoursText, 10); // 「時間」を除去して数値に変換
+}
 
-// 一応結果をコンソールに出力
-    console.log(`労働日数: ${workDays}日`);
-    console.log(`総勤務時間: ${totalWorkHours}時間`);
+function getAverageWorkHoursPerDay() {
+    const workDays = getWorkDays();
+    const totalWorkHours = getTotalWorkHours();
+    return totalWorkHours / workDays;
+}
 
-    const averageWorkHoursPerDay = totalWorkHours / workDays;
-    console.log(`労働時間平均: ${averageWorkHoursPerDay.toFixed(2)} 時間/日`);
-
-// 不足時間の横に平均労働時間を追加
+function addAverageWorkHoursElement() {
+    const averageWorkHoursPerDay = getAverageWorkHoursPerDay();
     const shortageTimeElement = document.querySelector(".items");
     const averageWorkHoursElement = document.createElement("div");
-    averageWorkHoursElement.innerHTML = `<div class="item"><div class="label">労働時間平均</div><div class="body"><span class="hour-min"><span class="hour-min__hour"><span class="hour-min__value">${averageWorkHoursPerDay.toFixed(
-        2
-    )}</span><span class="hour-min__unit">時間</span></span></span></div></div>`;
+    averageWorkHoursElement.innerHTML = `<div class="item"><div class="label">労働時間平均</div><div class="body"><span class="hour-min"><span class="hour-min__hour"><span class="hour-min__value">${averageWorkHoursPerDay.toFixed(2)}</span><span class="hour-min__unit">時間</span></span></span></div></div>`;
     shortageTimeElement.appendChild(averageWorkHoursElement);
-    // NOTE: mount 時に実行したいが、onloadではDOM がまだ生成されていないため、setTimeout で遅延させている
-}, 1000);
-
+}
